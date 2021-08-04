308d511c97c8551d174dce6855c0a69d339e5ac7
Crowdbotics <team@crowdbotics.com>
2021-08-04 14:31:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "royal_lab_29450Identifier",
+  url: "https://royal_lab_29450Identifier.botics.co",
+  api: "https://royal_lab_29450Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
